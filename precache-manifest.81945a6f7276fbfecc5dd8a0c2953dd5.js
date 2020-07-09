0e8f4e528d704a1a49525ea64d2fa775ab14e18a
Alen <alen.bedek@gmail.com>
2020-07-09 10:15:55 +0200

    Updates

---
+++ precache-manifest.81945a6f7276fbfecc5dd8a0c2953dd5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6dcae5cc248325347b3f6e550d7cac",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a4edcc573bcc659f2188",
+    "url": "/robofriends/static/css/main.f12f823d.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4edcc573bcc659f2188",
+    "url": "/robofriends/static/js/main.c3239f57.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
